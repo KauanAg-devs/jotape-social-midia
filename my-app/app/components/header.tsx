e64757c3a6c1e44a1b4c00d0f057f1e911709e07
kauanAg <117299925+KauanAg-devs@users.noreply.github.com>
2025-03-24 18:23:22 +0000

    refactor: search-input placeholder color at mobile screens

--- my-app/app/components/header.tsx
+++ my-app/app/components/header.tsx
@@ -3,17 +3,17 @@ import SearchIcon from "./search-icon";
 
 export default function Header() {
     return (
-        <header className="py-4 relative w-screen flex justify-between">
+        <header className="py-4 relative w-screen flex justify-between bg-white">
             <div></div>
 
             <div className="relative">
                 <input
-                type="text"
-                placeholder="Pesquisar..."
-                className="w-72 md:w-[25rem] lg:w-[35rem] h-12 px-4 py-2 border-0 rounded-full bg-white shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all"
+                    type="text"
+                    placeholder="Pesquisar..."
+                    className="w-72 md:w-[25rem] lg:w-[35rem] h-12 px-4 py-2 border-0 rounded-full bg-white shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 transition-all placeholder-gray-500 placeholder:opacity-70"
                 />
                 <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-blue-500 transition-colors">
-                <SearchIcon className="w-5 h-5" />
+                    <SearchIcon className="w-5 h-5" />
                 </button>
             </div>
 
